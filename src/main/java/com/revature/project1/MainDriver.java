f7b9c8a5df7486149412d3e277e51471fa247839
TwcL774 <tristanwright318@revature.net>
2022-12-25 21:08:15 -0500

    init commit

---
+++ src/main/java/com/revature/project1/MainDriver.java
@@ -0,0 +1,12 @@
+package com.revature.project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainDriver {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainDriver.class, args);
+	}
+}
