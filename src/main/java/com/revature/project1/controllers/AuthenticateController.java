39f0df52c0fecd6d934926066da62b2070e6b61c
TwcL774 <tristanwright318@revature.net>
2022-12-29 21:21:24 -0500

    added delay/error testing

--- src/main/java/com/revature/project1/controllers/AuthenticateController.java
+++ src/main/java/com/revature/project1/controllers/AuthenticateController.java
@@ -1,10 +1,13 @@
 package com.revature.project1.controllers;
 
+import java.util.Random;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.revature.project1.models.User;
 import com.revature.project1.models.UsernamePasswordAuthentication;
 import com.revature.project1.services.UserService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class AuthenticateController {
@@ -38,6 +42,28 @@ public class AuthenticateController {
 
         session.invalidate();
         return new ResponseEntity<>("Successfully logged out", HttpStatus.OK);
+    }
+
+    @GetMapping("/delay")
+    public ResponseEntity<String> getDelay() {
+
+        Random random = new Random();
+        long minDelay = 200;
+        long maxDelay = 400;
+        long delay = random.nextInt((int) (maxDelay - minDelay + 1)) + minDelay;
+
+        try {
+            Thread.sleep(delay);
+        } catch (InterruptedException e) {
+        }
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @GetMapping("/error")
+    public ResponseEntity<String> getError() {
+
+        return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
 }
