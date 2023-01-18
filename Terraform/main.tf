33759a07f3d3cc469692ab441f8cacbe423d41d6
TwcL774 <tristanwright318@revature.net>
2023-01-18 16:58:26 -0500

    aws rds through terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,7 +2,7 @@
 # provider in our terraform.tf file, where we defined our required_provder
 provider "aws" {
   region     = "us-east-1"
-  profile = "terraform"
+  # profile = "terraform"
   access_key = var.access_key
   secret_key = var.secret_key
   # both keys should be provided by an IAM role created in aws
@@ -26,15 +26,50 @@ provider "aws" {
 #   }
 # }
 
-resource "aws_db_instance" "default" {
-  allocated_storage    = 10
+module "db" {
+  source  = "terraform-aws-modules/rds/aws"
+
+  identifier = "teamsol"
+
+  engine            = "postgres"
+  engine_version    = "13.7"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 10
   max_allocated_storage = 20
-  db_name              = "panetarium"
-  engine               = "postgresql"
-  engine_version       = "13.7-R1"
-  instance_class       = "db.t3.micro"
-  vpc_security_group_ids = "rds-access"
-  username             = var.access_key
-  password             = var.secret_key
-  skip_final_snapshot  = true
+
+  db_name  = "teamsol"
+  username = "terraform"
+  port     = "5432"
+
+  iam_database_authentication_enabled = true
+
+  vpc_security_group_ids = ["sg-02094add46cdfe497"]
+
+  # maintenance_window = "Mon:00:00-Mon:03:00"
+  # backup_window      = "03:00-06:00"
+
+  # Enhanced Monitoring - see example for details on how to create the role
+  # by yourself, in case you don't want to create it automatically
+  # monitoring_interval = "30"
+  # monitoring_role_name = "MyRDSMonitoringRole"
+  # create_monitoring_role = true
+
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
+
+  # DB subnet group
+  # create_db_subnet_group = true
+  # subnet_ids             = ["subnet-12345678", "subnet-87654321"]
+
+  # DB parameter group
+  family = "postgres13"
+
+  # DB option group
+  major_engine_version = "13.7"
+
+  # Database Deletion Protection
+  deletion_protection = false
+
 }
