f612c0af2d9203a0fa6709ff184e397a253f5b9e
TwcL774 <tristanwright318@revature.net>
2023-01-18 22:19:50 -0500

    updated terraform files

--- Terraform/terraform.tf
+++ Terraform/terraform.tf
@@ -1,18 +1,8 @@
 terraform {
   required_providers {
-    # aws is the name we are giving our required provider: its configuration details is handled by the "source"
-    # in this case, hashicorp/aws
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.0"
     }
   }
-
-  # backend "s3" {
-  #   bucket = "revature-team-sol-terraform-state-bucket"
-  #   profile = "terraform"
-  #   region = "us-east-1"
-  #   key = "state/terraform.tfstate"
-  #   encrypt = true
-  # }
 }
