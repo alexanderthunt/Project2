271cab16756e6d47ad0fe92c57eafb9f58584157
SabrinaRSmith <sabrina813@revature.net>
2023-01-19 14:05:50 -0600

    added s3 bucket

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = var.access_key
-  secret_key = var.secret_key
+  profile = "team-sol"
+  # access_key = var.access_key
+  # secret_key = var.secret_key
 }
 
 module "rds" {
@@ -22,4 +23,23 @@ module "rds" {
   publicly_accessible    = true
   skip_final_snapshot    = true
   create_random_password = false
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  # if you get a "wrong region" error there is a good chance it is because of your bucket name
+  bucket = "revature-team-sol-bucket" # this bucket name has already been taken, leaving it for now to show error message
+  lifecycle {
+    prevent_destroy = false # this makes sure terraform does not destroy the bucket
+  }
+  versioning {
+    enabled = true # this makes the bucket keep track of the various objects placed inside
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+  
 }
